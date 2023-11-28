33967b4280982906169a3bc6944b7a6bf3154e0d
Venoninho23 <mauriciolucas416@gmail.com>
2023-11-28 19:49:43 -0300

    laboratorio 04

--- src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
+++ src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
@@ -3,6 +3,10 @@ package br.fepi.socialbooks.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +15,24 @@ import br.fepi.socialbooks.model.Livro;
 import br.fepi.socialbooks.repository.LivrosRepository;
 
 @RestController
+@RequestMapping(value = "/livros")
 public class LivrosResources { 
 	
 	@Autowired
 	private LivrosRepository livrosRepository;
 
-	@RequestMapping(value = "/livros", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Livro> listar() {
 		return livrosRepository.findAll();
 	}
 
+	@PostMapping()
+	public Livro salvar(@RequestBody Livro livro){
+		return livrosRepository.save(livro);
+	}
+
+	@GetMapping(value = "/{id}")
+	public Livro findById(@PathVariable (value = "id")Long id){
+		return livrosRepository.findById(id).orElse(null);
+	}
 }
