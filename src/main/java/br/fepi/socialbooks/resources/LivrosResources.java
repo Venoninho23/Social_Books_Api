6a1c9cd23ece659e50555d5e870ed7624f3e40e3
Venoninho23 <mauriciolucas416@gmail.com>
2023-11-28 19:51:03 -0300

    Laboratorio 05

--- src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
+++ src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
@@ -3,9 +3,11 @@ package br.fepi.socialbooks.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,5 +36,16 @@ public class LivrosResources {
 	@GetMapping(value = "/{id}")
 	public Livro findById(@PathVariable (value = "id")Long id){
 		return livrosRepository.findById(id).orElse(null);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public void deletar(@PathVariable(value = "id")Long id){
+		livrosRepository.deleteById(id);
+	}
+
+	@PutMapping(value = "{id}")
+	public void atualizar(@RequestBody Livro livro, @PathVariable("id") Long id){
+		livro.setId(id);
+		livrosRepository.save(livro);
 	}
 }
